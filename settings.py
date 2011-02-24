2253a028c30b6480a9ed561735c2eeb696b0d0ef
Saúl Moncada <saul@kentriki.com>
2011-02-23 22:45:37 -0430

    Arreglos pasee prod

--- settings.py
+++ settings.py
@@ -22,6 +22,18 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#     'default': {
+#         'ENGINE': 'postgresql_psycopg2', #, 'postgresql', 'mysql', 'sqlite3' or 'oracle'.                                          
+#         'NAME': 'gestion',                      # Or path to database file if using sqlite3.                                       
+#         'USER': 'gestion',                      # Not used with sqlite3.                                                           
+#         'PASSWORD': 'ieQu0Xae',                  # Not used with sqlite3.                                                          
+#         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.                               
+#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.                                 
+#     }
+# }
+
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
