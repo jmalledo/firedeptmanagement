895051e20569c71ac36eec2b73d226325795124a
Saúl Moncada <saul@kentriki.com>
2011-05-31 23:49:41 -0430

    Arreglo acentos en capitalrelacional

--- capitalrelacional/search_indexes.py
+++ capitalrelacional/search_indexes.py
@@ -4,6 +4,7 @@ from haystack.indexes import RealTimeSearchIndex, CharField
 from haystack import site
 from capitalrelacional.models import RelationalPerson, RelationalCompany
 from personal.models import Firefigther
+import unicodedata
 
 class RelatedIndex(RealTimeSearchIndex):
     text = CharField(document=True)
@@ -18,27 +19,27 @@ class RelatedIndex(RealTimeSearchIndex):
         self.prepared_data = super(RelatedIndex, self).prepare(instance)
         data = self.prepared_data
         if type(instance)  == RelationalPerson:
-            data['text'] = "%s %s %s %s %s" % (instance.first_name, instance.first_name_2, instance.last_name, instance.last_name_2, instance.id_document)
-            data["name"] = "%s %s" % (instance.first_name, instance.last_name)
+            data['text'] = self.strip_accents("%s %s %s %s %s" % (instance.first_name, instance.first_name_2, instance.last_name, instance.last_name_2, instance.id_document))
+            data["name"] = self.strip_accents("%s %s" % (instance.first_name, instance.last_name))
             data['model'] = 'RelationalPerson'
             
-            data['type'] = instance.type
+            data['type'] = self.strip_accents(instance.type)
             data['type_company'] = "L"
             phones = instance.persontelephonenumber_set.filter(main=True)
             data['telephone'] = str(phones[0]) if len(phones) > 0 else "" 
             data['email'] = instance.primary_email
         elif type(instance)  == RelationalCompany:
-            data['text'] = "%s %s" % (instance.name, instance.rif)
-            data["name"] = "%s" % instance.name
+            data['text'] = self.strip_accents("%s %s" % (instance.name, instance.rif))
+            data["name"] = self.strip_accents("%s" % instance.name)
             data['model'] = 'RelationalCompany'
-            data['type'] = instance.type
+            data['type'] = self.strip_accents(instance.type)
             data['type_company'] = instance.typecompany
             phones = instance.companytelephonenumber_set.filter(main=True)
             data['telephone'] =  str(phones[0]) if len(phones) > 0 else ""
             data['email'] = instance.website
         elif type(instance)  == Firefigther:
-            data['text'] = "%s %s %s %s %s %d" % (instance.first_name, instance.first_name_2, instance.last_name, instance.last_name_2, instance.id_document, instance.number)
-            data["name"] = "%s %s" % (instance.first_name, instance.last_name)
+            data['text'] = self.strip_accents("%s %s %s %s %s %d" % (instance.first_name, instance.first_name_2, instance.last_name, instance.last_name_2, instance.id_document, instance.number))
+            data["name"] = self.strip_accents("%s %s" % (instance.first_name, instance.last_name))
             data['model'] = 'Firefigther'
             phones = instance.persontelephonenumber_set.filter(main=True)
             data['telephone'] = str(phones[0]) if len(phones) > 0 else "" 
@@ -46,6 +47,9 @@ class RelatedIndex(RealTimeSearchIndex):
             data['type'] = "B"
             data['type_company'] = 'B'
         return data
+
+    def strip_accents(self, s):
+        return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
     
 
 site.register(RelationalCompany, RelatedIndex)
